9d8c72315639b34f8d7a2b99bb1af69177a2dcf1
Qeatrix <elitelektro74@gmail.com>
2025-12-25 05:00:24 +0500

    Remove cargo generated stuff

--- target/debug/deps/uikit-adf6713add1c6d77.d
+++
@@ -1,5 +0,0 @@
-/home/quartix/Projects/iced-uikit/uikit/target/debug/deps/uikit-adf6713add1c6d77.d: src/lib.rs
-
-/home/quartix/Projects/iced-uikit/uikit/target/debug/deps/libuikit-adf6713add1c6d77.rmeta: src/lib.rs
-
-src/lib.rs:
